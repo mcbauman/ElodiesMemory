85ca4b03dca0bb510a81a28c7c2ceefa36c94ed9
mcbauman <m.c.baumann@icloud.com>
2022-03-18 20:44:59 +0100

    uncovering cards works

--- main.js
+++ main.js
@@ -1,27 +1,12 @@
+
+//CREATING THE PAGE
 const Emojis=["😁","🤪","💩","😈","💪","🏊","🚴","🙈","🌍","🌈","🍆","✈","🏡","🌅","⌚","📱","💻","📷","😁","🤪","💩","😈","💪","🏊","🚴","🙈","🌍","🌈","🍆","✈","🏡","🌅","⌚","📱","💻","📷"]
 const numbers=shuffle([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35])
-console.log(numbers);
-
-// CONSOLE LOG----
-// console.log("Starting Emojis lenght",Emojis.length);
-
-// function randomNumberF(){
-//     let randomNumber=Math.floor((Math.random()*19)+1)
-    
-//     if(numbers.includes(randomNumber)){
-//         numbers.splice(randomNumber,1)
-//         return randomNumber
-//     }
-//     else{
-//         console.log("YYY---ExtraCall---YYY");
-//         randomNumberF();
-//     }
-// }
 
 function createDiv(i){
     const div=document.createElement("div");
-    div.className="outer"
-    div.innerHTML=`<div className='inner'>${Emojis[i]}</div>`;
+    div.class="outer"
+    div.innerHTML=`<div onlcik='clickFunction' class='inner'>${Emojis[i]}</div>`;
 
     document.getElementById("main").appendChild(div);
 }
@@ -43,3 +28,10 @@ repeat(createDiv,36)
   }
   return array;
 }
+
+//SETTING THE RULES AND FUNCTIOPNS
+function clickFunction(e){
+    console.log(e.target);
+    e.target.children[0].style.visibility="visible";
+}
+document.getElementById("main").addEventListener("click",clickFunction)
